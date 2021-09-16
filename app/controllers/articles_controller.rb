018b9c48164838553005869eb557c1046fb662bc
danilo.costa <danilo.costa@olist.com>
2021-09-16 15:49:08 -0300

    refactir(articles_controller): object has been removed from methods.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,17 +8,13 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
   def new
     @article = Article.new
   end
 
   def create
-    @article = Article.new(article_params)
-
     if @article.save
       redirect_to @article
     else
@@ -26,13 +22,9 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
       redirect_to @article
     else
@@ -41,7 +33,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     redirect_to root_path
